36e76f0b522a3523745071fdf8c672ade30ae01e
Vaibhav Bali <vbali@hub24.com.au>
2018-05-28 15:42:28 +1000

    LinkedList

---
+++ LinkedList/SinglyLinkedList.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("TestProject")]
+
+namespace LinkedList
+{
+    public class SinglyLinkedList<T> : ICollection<T>
+    {
+        internal class Node
+        {
+            public Node(T value)
+            {
+                Value = value;
+            }
+            public T Value { get; }
+            public Node Next { get; set; }
+
+            public override string ToString()
+            {
+                return Value.ToString();
+            }
+        }
+
+        internal Node Head { get; private set; }
+        internal Node Tail { get; private set; }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var currentNode = Head;
+            while (currentNode != null)
+            {
+                yield return currentNode.Value;
+                currentNode = currentNode.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            var nodeToAdd = new Node(item);
+            if (Tail == null)
+            {
+                Head = Tail = nodeToAdd;
+            }
+            else
+            {
+                Tail.Next = nodeToAdd;
+                Tail = nodeToAdd;
+            }
+
+            ++Count;
+        }
+
+        public void Clear()
+        {
+            Head = Tail = null;
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            var currentNode = Head;
+            while (currentNode != null)
+            {
+                if (currentNode.Value.Equals(item))
+                {
+                    return true;
+                }
+
+                currentNode = currentNode.Next;
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            var currentNode = Head;
+            while (currentNode != null)
+            {
+                array[arrayIndex++] = currentNode.Value;
+                currentNode = currentNode.Next;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            var currentNode = Head;
+            Node previous = null;
+            while (currentNode != null)
+            {
+                if (currentNode.Value.Equals(item))
+                {
+                    //first item could be head
+                    if (currentNode == Head)
+                    {
+                        //Only one item 
+                        if (Head == Tail)
+                        {
+                            Clear();
+                        }
+                        else
+                        {
+                            Head = currentNode.Next;
+                        }
+                    } //item is tail
+                    else if (currentNode == Tail)
+                    {
+                        Tail = previous;
+                        Debug.Assert(Tail != null, nameof(Tail) + " != null");
+                        Tail.Next = null;
+                    }
+                    else
+                    {
+                        Debug.Assert(previous != null, nameof(previous) + " != null");
+                        previous.Next = currentNode.Next;
+                    }
+
+                    --Count;
+                    return true;
+                }
+
+                previous = currentNode;
+                currentNode = currentNode.Next;
+            }
+
+            return false;
+        }
+
+        public int Count { get; private set; }
+        public bool IsReadOnly => false;
+    }
+}
---
+++ TestProject/SinglyLinkedListTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using LinkedList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject
+{
+    [TestClass]
+    public class SinglyLinkedListTests
+    {
+        [TestMethod]
+        public void AssertCountZero()
+        {
+            var linkedList = new SinglyLinkedList<int>();
+            Assert.AreEqual(0, linkedList.Count);
+        }
+
+        [TestMethod]
+        public void AssertHeadTailNull()
+        {
+            var linkedList = new SinglyLinkedList<int>();
+            Assert.IsNull(linkedList.Head);
+            Assert.IsNull(linkedList.Tail);
+        }
+
+        [TestMethod]
+        public void RemoveHeadAssertHeadIsTwo()
+        {
+            var linkedList = new SinglyLinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            linkedList.Add(4);
+
+            linkedList.Remove(1);
+            Assert.AreEqual(2, linkedList.Head.Value);
+            Assert.AreEqual(3, linkedList.Count);
+
+        }
+
+
+        [TestMethod]
+        public void RemoveTailAssertTailIs3()
+        {
+            var linkedList = new SinglyLinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            linkedList.Add(4);
+
+            linkedList.Remove(4);
+            Assert.AreEqual(3, linkedList.Tail.Value);
+            Assert.AreEqual(3, linkedList.Count);
+        }
+
+        [TestMethod]
+        public void RemoveMiddleAssertHeadAndTailUnchanged()
+        {
+            var linkedList = new SinglyLinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            linkedList.Add(4);
+
+            var head = linkedList.Head;
+            var tail = linkedList.Tail;
+
+            linkedList.Remove(2);
+            Assert.AreEqual(head, linkedList.Head);
+            Assert.AreEqual(tail, linkedList.Tail);
+            Assert.AreEqual(3, linkedList.Count);
+        }
+
+        [TestMethod]
+        public void Iteration()
+        {
+            var linkedList = new SinglyLinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            linkedList.Add(4);
+
+            var item = 1;
+            foreach (var i in linkedList)
+            {
+                Assert.AreEqual(item, i);
+                item++;
+            }
+
+        }
+
+    }
+}
