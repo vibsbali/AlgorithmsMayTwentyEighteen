53b8b8c29e506324e60856ba4489cadf8e29b1fd
Vaibhav Bali <vbali@hub24.com.au>
2018-05-31 16:37:15 +1000

    Implemented doubly linked list

---
+++ LinkedList/DoublyLinkedList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    public class DoublyLinkedList<T> : ICollection<T>
+    {
+        internal class Node
+        {
+            internal T Value { get; private set; }
+            internal Node Next { get; set; }
+
+            public Node(T value)
+            {
+                Value = value;
+            }
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count { get; }
+        public bool IsReadOnly { get; }
+    }
+}
